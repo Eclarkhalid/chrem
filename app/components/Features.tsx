478331ea377dceceb60ca3f8729db0f1b4534c2a
Eclarkhalid <eclarkhalid@gmail.com>
2023-10-21 13:42:06 +0300

    updating

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -1,5 +1,6 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { services } from "@/constants";
+import Image from "next/image";
 
 const Features = () => {
   return <>
@@ -15,12 +16,15 @@ const Features = () => {
                 <p className="max-w-[600px] text-slate-500 md:text-xl dark:text-zinc-100 mx-auto">
                   Our attributes are crafted to elevate your efficiency and simplify your processes.
                 </p>
-                <div className="flex justify-center flex-wrap gap-9 p-6">
+                <div className="flex justify-center flex-wrap gap-9 py-6">
                   {services.map((service) => (
                     <Card key={service.label} className='flex-1 sm:w-[300px] sm:min-w-[300px] w-full rounded-[20px] shadow-3xl px-10 max-sm:px-2 py-8 bg-gray-200'>
-                      <CardHeader className="font-bold text-2xl">
-                        {service.label}
+                      <CardHeader className=' flex justify-center items-center  rounded-full'>
+                          <Image src={service.img} alt={service.label} width={40} height={40} className="font-bold" />
                       </CardHeader>
+                      <CardTitle className="font-bold text-2xl">
+                        {service.label}
+                      </CardTitle>
                       <CardContent>
                         {service.subtext}
                       </CardContent>
--- constants/index.ts
+++ constants/index.ts
@@ -1,13 +1,20 @@
+import img1 from '../public/microchip.png'
+import img2 from '../public/fast-food.png'
+import img3 from '../public/video.png'
+
 export const services = [
   {
+    img: img1,
     label: "Fast Recipe Delivery",
     subtext: "Enjoy seamless shopping with our fast and reliable recipe delivery service."
   },
   {
+    img: img2,
     label: "Recipe Inspiration",
     subtext: "Discover new and exciting recipes to try, curated by our team of culinary experts."
   },
   {
+    img: img3,
     label: "Dedicated Support",
     subtext: "Our dedicated team is here to assist you with any recipe questions or needs you may have."
   },
